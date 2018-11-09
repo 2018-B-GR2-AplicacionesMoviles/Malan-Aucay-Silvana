ce0ff5746e7e211e01cc6308f2de4ba4dd39b32e
MALANSILVANA <silvana.malan@epn.edu.ec>
2018-11-09 16:02:42 -0500

    intent

---
+++ miprimeraaplicacion/app/src/main/java/com/example/usrdel/miprimeraaplicacion/ButtonActivity.kt
@@ -0,0 +1,46 @@
+package com.example.usrdel.a2018movilesgr2
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.usrdel.miprimeraaplicacion.R
+import kotlinx.android.synthetic.main.activity_button.*
+
+class ButtonActivity : AppCompatActivity() {
+
+    var nombre: String=""
+    var apellido: String =""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button)
+        this.capturarDatosIntent()
+        act_but_nombre_apellido.text = "${nombre} ${apellido}"
+
+        but_but_intent_implicito.setOnClickListener(enviarIntentCorreo)
+
+    }
+
+    fun capturarDatosIntent(){
+        this.nombre = intent.getStringExtra("nombre")
+
+        this.apellido = intent.getStringExtra("apellido")
+
+        fun enviarIntentCorreo(){
+
+            val correo = text_correo.text
+            val subject = text_subject.text
+            val texto = texto_text.text
+
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/html"
+
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(correo,"s@s.com"))
+            intent.putExtra(Intent.EXTRA_SUBJECT,subject)
+            intent.putExtra(Intent.EXTRA_TEXT,texto)
+
+            startActivity(intent)
+        }
+
+    }
+}
--- miprimeraaplicacion/app/src/main/java/com/example/usrdel/miprimeraaplicacion/MainActivity.kt
+++ miprimeraaplicacion/app/src/main/java/com/example/usrdel/miprimeraaplicacion/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.usrdel.miprimeraaplicacion
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.os.Parcelable
+import android.util.Log
+import com.example.usrdel.a2018movilesgr2.ButtonActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        boton_navegar
+            .setOnClickListener {
+                /*
+                Log.i("navegacion","Hola")
+                Log.w("navegacion","Hola")
+                Log.d("navegacion","Hola")
+                Log.e("navegacion","Hola")
+                Log.v("navegacion","Hola")
+                */
+                this.irAPantallaDeBotones()
+            }
     }
+
+    fun irAPantallaDeBotones() {
+        // INTENT
+        val intentIrABotones = Intent(this, ButtonActivity::class.java)
+        intentIrABotones.putExtra("nombre","Silvana")
+        intentIrABotones.putExtra("apellido","Malan")
+        this.startActivity(intentIrABotones)
+    }
+
 }
