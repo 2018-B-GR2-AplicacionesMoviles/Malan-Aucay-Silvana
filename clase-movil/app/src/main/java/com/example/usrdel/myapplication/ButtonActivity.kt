363c94355698cfaca642a6ebb5ffe147d3695e46
MALANSILVANA <silvana.malan@epn.edu.ec>
2019-01-03 15:59:51 -0500

    parte1

---
+++ clase-movil/app/src/main/java/com/example/usrdel/myapplication/ButtonActivity.kt
@@ -0,0 +1,15 @@
+package com.example.usrdel.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_button.*
+
+class ButtonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button)
+
+    }
+}
